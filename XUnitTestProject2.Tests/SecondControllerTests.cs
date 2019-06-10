54c185cb8a3e22874603736d3c62f28535d84a9e
mega24cde <megan.denman@gmail.com>
2019-06-10 14:53:46 -0400

    second commit

---
+++ XUnitTestProject2.Tests/SecondControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using WebApplication2.Controllers;
+
+namespace WebApplication2.Tests
+{
+    class SecondController
+    {
+	[Fact]
+	public void Index_Returns_Funky_Skunky()
+        {
+            var underTest = new SecondController();
+            var result = underTest();
+            Assert.Equal("Funky Skunk", result);
+        }
+
+
+    }
+}
